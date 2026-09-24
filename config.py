31acae9eb75dcff285bb3abe5b7457ab2eaee0dd
zzz3210 <a17677309183@163.com>
2026-09-24 22:19:58 +0800

    Change data path to synthetic_dataset

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 from model.MSTNet import MSTNet
 
-data_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "data"))
+data_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "synthetic_dataset"))
 
 used_model = MSTNet
 
